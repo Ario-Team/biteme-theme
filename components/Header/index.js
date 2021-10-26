bda7d6f43a359094644bfaf5a0fc0a11c916f58c
MohammadBagheri <71064028+mohammad24680@users.noreply.github.com>
2021-10-26 15:46:29 +0330

    Created the header styles.
    Created the header styles and make them responsive.

--- components/Header/index.js
+++ components/Header/index.js
@@ -2,55 +2,58 @@ import Link from "next/link";
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClock as farClock } from "@fortawesome/free-solid-svg-icons";
+import HeaderStyle from "styles/components/Header.module.css";
 
 const Header = () => {
   return (
-    <header>
-      <div>
-        <div>
-          <a>
+    <header className={HeaderStyle.header}>
+      <div className={HeaderStyle.head_top}>
+        <div className={HeaderStyle.headtop_left}>
+          <a className={HeaderStyle.a_global}>
             <FontAwesomeIcon icon={farClock} />
             <span>Mon to Sat : 08:00 - 15:30</span>
           </a>
         </div>
-        <div>
-          <a href="tel:+1801454587">+1 801 4545 87</a>
-          <a href="mailto:contact@gmail.com">contact@gmail.com</a>
+        <div className={HeaderStyle.headtop_bottom}>
+          <a className={HeaderStyle.a_global} href="tel:+1801454587">+1 801 4545 87</a>
+          <a className={HeaderStyle.a_global} href="mailto:contact@gmail.com">contact@gmail.com</a>
         </div>
       </div>
-      <div>
-        <Link href="/" passHref={true}>
-          <Image
-            src="/images/logo-icon.png"
-            width="100%"
-            height="100%"
-            alt="Logo"
-          />
-        </Link>
-      </div>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link href="/">HOME</Link>
-            </li>
-            <li>
-              <Link href="/about">ABOUT US</Link>
-            </li>
-            <li>
-              <Link href="/offer">OFFER</Link>
-            </li>
-            <li>
-              <Link href="/menu">MENU</Link>
-            </li>
-            <li>
-              <Link href="/gallery">GALLERY</Link>
-            </li>
-            <li>
-              <Link href="/contact">CONTACT</Link>
-            </li>
-          </ul>
-        </nav>
+      <div className={HeaderStyle.head_bottom}>
+        <div className={HeaderStyle.head_logo}>
+          <Link  href="/" passHref={true}>
+            <Image
+              src="/images/logo-icon.png"
+              width="100%"
+              height="100%"
+              alt="Logo"
+            />
+          </Link>
+        </div>
+        <div className={HeaderStyle.head_menu}>
+          <nav>
+            <ul>
+              <li>
+                <Link href="/">HOME</Link>
+              </li>
+              <li>
+                <Link href="/about">ABOUT US</Link>
+              </li>
+              <li>
+                <Link href="/offer">OFFER</Link>
+              </li>
+              <li>
+                <Link href="/menu">MENU</Link>
+              </li>
+              <li>
+                <Link href="/gallery">GALLERY</Link>
+              </li>
+              <li>
+                <Link href="/contact">CONTACT</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
     </header>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Header from "components/Header";
-import indexStyles from "styles/index.module.css";
+import indexStyles from "styles/pages/index.module.css";
 
 export default function Home() {
   return (
