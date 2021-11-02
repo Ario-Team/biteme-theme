eb2c992b4f8753e10f1889690d0a3f38449eed44
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2021-11-02 11:38:53 +0330

    Added the OfferBox component

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import Head from "next/head";
 import Header from "components/Header";
 import indexStyles from "styles/pages/index.module.scss";
 import AboutUs from "components/AboutUs";
+import OfferBox from "components/OfferBox";
 
 export default function Home() {
   return (
     <main className={indexStyles.container}>
       <Header />
       <AboutUs />
+      <OfferBox />
     </main>
   );
 }
