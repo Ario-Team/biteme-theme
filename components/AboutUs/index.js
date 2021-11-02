1144ce156823fdb53c27a8d5b290a75ecd2a19a8
MohammadBagheri <71064028+mohammad24680@users.noreply.github.com>
2021-11-02 17:45:40 +0330

    Added the AboutUS component responsive and stylesheet.

--- components/AboutUs/index.js
+++ components/AboutUs/index.js
@@ -7,59 +7,61 @@ import animationController from "./utils/animationController";
 const AboutUs = () => {
   return (
     <div className={AboutUsStyles.container}>
-      <InView
-        className={AboutUsStyles.animations_container}
-        onChange={animationController}
-        threshold={[0.05, 0.4]}
-      >
-        <div>
-          <figure>
-            <Image
-              src="/images/aboutus/element-1.png"
-              alt=""
-              width="351px"
-              height="438px"
-            />
-          </figure>
-          <figure>
-            <Image
-              src="/images/aboutus/element-2.png"
-              alt=""
-              width="206px"
-              height="206px"
-            />
-          </figure>
-        </div>
-        <div>
-          <figure>
-            <Image
-              src="/images/aboutus/element-3.png"
-              alt=""
-              width="289px"
-              height="304px"
-            />
-          </figure>
-          <figure>
-            <Image
-              src="/images/aboutus/element-4.png"
-              alt=""
-              width="194px"
-              height="146px"
-            />
-          </figure>
-        </div>
-      </InView>
       <div className={AboutUsStyles.information_container}>
+        <InView
+          className={AboutUsStyles.animations_container}
+          onChange={animationController}
+          threshold={[0.05, 0.4]}
+        >
+          <div>
+            <figure>
+              <Image
+                src="/images/aboutus/element-1.png"
+                alt=""
+                width="351px"
+                height="438px"
+              />
+            </figure>
+            <figure>
+              <Image
+                src="/images/aboutus/element-2.png"
+                alt=""
+                width="206px"
+                height="206px"
+              />
+            </figure>
+          </div>
+          <div>
+            <figure>
+              <Image
+                src="/images/aboutus/element-3.png"
+                alt=""
+                width="289px"
+                height="304px"
+              />
+            </figure>
+            <figure>
+              <Image
+                src="/images/aboutus/element-4.png"
+                alt=""
+                width="194px"
+                height="146px"
+              />
+            </figure>
+          </div>
+        </InView>
         <div className={AboutUsStyles.information_box}>
           <h2 className={AboutUsStyles.heading_top}>Our story</h2>
           <h1 className={AboutUsStyles.heading_bottom}>About us</h1>
-          <Image
-            src="/images/aboutus/heading-element.png"
-            width="171px"
-            height="21px"
-            alt=""
-            className={AboutUsStyles.heading_subimage}
-          />
+          <figure className={AboutUsStyles.heading_subfigure}>
+            <Image
+              src="/images/aboutus/heading-element.png"
+              width="171px"
+              height="21px"
+              alt=""
+              className={AboutUsStyles.heading_subimage}
+            />
+          </figure>
           <div className={AboutUsStyles.heading_content}>
             <span className={AboutUsStyles.heading_content_header}>
               Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -79,9 +81,10 @@ const AboutUs = () => {
               <div className={AboutUsStyles.badge_item}>
                 <figure className={AboutUsStyles.badge_item_icon}>
                   <Image
+                    className={AboutUsStyles.badge_img}
                     src="/images/aboutus/boxes-icon/feature1.png"
                     width="65px"
-                    height="55"
+                    height="55px"
                     alt=""
                   />
                 </figure>
@@ -94,11 +97,12 @@ const AboutUs = () => {
               <div className={AboutUsStyles.badge_item}>
                 <figure className={AboutUsStyles.badge_item_icon}>
                   <Image
+                    className={AboutUsStyles.badge_img}
                     src="/images/aboutus/boxes-icon/feature2.png"
                     width="65px"
-                    height="55"
+                    height="55px"
                     alt=""
-                  />{" "}
+                  />
                 </figure>
                 <h5 className={AboutUsStyles.badge_item_title}>
                   Freshly cooked
@@ -109,11 +113,12 @@ const AboutUs = () => {
               <div className={AboutUsStyles.badge_item}>
                 <figure className={AboutUsStyles.badge_item_icon}>
                   <Image
+                    className={AboutUsStyles.badge_img}
                     src="/images/aboutus/boxes-icon/feature3.png"
                     width="65px"
-                    height="55"
+                    height="55px"
                     alt=""
-                  />{" "}
+                  />
                 </figure>
                 <h5 className={AboutUsStyles.badge_item_title}>
                   24 Hourse support
@@ -123,12 +128,15 @@ const AboutUs = () => {
           </div>
         </div>
         <div className={AboutUsStyles.chief_container}>
-          <Image
-            src="/images/aboutus/chef.png"
-            width="457px"
-            height="769px"
-            alt=""
-          />
+          <figure className={AboutUsStyles.chief_image}>
+            <Image
+              src="/images/aboutus/chef.png"
+              layout="fill"
+              width="457px"
+              height="769px"
+              alt=""
+            />
+          </figure>
         </div>
       </div>
     </div>
