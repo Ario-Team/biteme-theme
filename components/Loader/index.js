ae2be79ba18572a69899a301a923e18d0a1c8ef7
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2021-11-07 14:45:30 +0330

    Add the Loader to the webpage

---
+++ components/Loader/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Styles from "styles/components/Loader/Loader.module.scss";
+
+const Loader = ({ isLoading }) => {
+  return (
+    <div className={isLoading ? Styles.loader : Styles.display_none}>
+      <figure className={Styles.loader_image}></figure>
+    </div>
+  );
+};
+
+export default Loader;
--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,29 @@ import "animate.css/animate.min.css";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import Loader from "components/Loader";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const handleStart = (url) => {
+      url !== router.pathname ? setLoading(true) : setLoading(false);
+    };
+    const handleComplete = (url) => setLoading(false);
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+  }, [router]);
+  return (
+    <>
+      <Loader isLoading={loading} />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
