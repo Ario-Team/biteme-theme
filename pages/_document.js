ab83fd63e630174f8a3c2c1a9169de9ca5d1d3d4
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2021-11-10 14:42:29 +0330

    Remove utf8 meta tag

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta charSet="utf-8" />
           <meta name="description" content="Food Website" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="keywords" content="food" />
