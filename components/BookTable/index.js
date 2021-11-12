7340f8d9cd8c77f57c6d2676bcf31e51bb8f053d
MohammadBagheri <71064028+mohammad24680@users.noreply.github.com>
2021-11-13 02:07:20 +0330

    Added the Book Table component responsive and stylesheet

--- components/BookTable/index.js
+++ components/BookTable/index.js
@@ -12,41 +12,58 @@ const BookTable = () => {
         <h2 className={BookTableStyles.heading2}>Reservation</h2>
         <h1 className={BookTableStyles.heading1}>Book a table</h1>
         <div className={BookTableStyles.heading_image_container}>
-          <figure>
+          <figure className={BookTableStyles.heading_image}>
             <Image
               src="/images/aboutus/heading-element.png"
               alt=""
               width="171px"
               height="21px"
             />
-            <div className={BookTableStyles.dates}>
-              <div>
-                <span>Mon - Fri:</span> 10am - 10pm
-              </div>
-              <div>
-                <span>Sat - Sun:</span> 9am - 12pm
-              </div>
-            </div>
           </figure>
+          <div className={BookTableStyles.dates}>
+            <div>
+              <span>Mon - Fri:</span> 10am - 10pm
+            </div>
+            <div>
+              <span>Sat - Sun:</span> 9am - 12pm
+            </div>
+          </div>
           <div className={BookTableStyles.form}>
             <form action="#">
-              <input type="text" name="name" placeholder="Name" />
-              <input type="email" name="email" placeholder="Email" />
-              <input type="number" name="phone" placeholder="Phone" />
-              <input
-                type="number"
-                name="numberOfPeople"
-                placeholder="No. of people"
-              />
-              <input type="date" name="date" placeholder="YY-MM-DD" />
-              <select>
-                <option>Please select</option>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-              </select>
-              <textarea placeholder="Message"></textarea>
-              <button type="submit">
+              <div className={BookTableStyles.form_booktable}>
+                <input type="text" name="name" placeholder="Name" />
+              </div>
+              <div className={BookTableStyles.form_booktable}>
+                <input type="email" name="email" placeholder="Email" />
+              </div>
+              <div className={BookTableStyles.form_booktable}>
+                <input type="number" name="phone" placeholder="Phone" />
+              </div>
+              <div className={BookTableStyles.form_booktable}>
+                <input
+                  type="number"
+                  name="numberOfPeople"
+                  placeholder="No. of people"
+                />
+              </div>
+              <div className={BookTableStyles.form_booktable}>
+                <input type="date" name="date" placeholder="YY-MM-DD" />
+              </div>
+              <div className={BookTableStyles.form_booktable}>
+                <select>
+                  <option>Please select</option>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                </select>
+              </div>
+              <div className={BookTableStyles.form_booktable_message}>
+                <textarea placeholder="Message"></textarea>
+              </div>
+              <button
+                type="submit"
+                className={BookTableStyles.form_booktable_button}
+              >
                 Book Table
                 <FontAwesomeIcon icon={faArrowRight} />
               </button>
