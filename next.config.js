8494d9cf720d33b89a3ebc356cd53b75cca37dab
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2021-12-04 12:54:41 +0330

    Disabled the swcMinify option for the nextjs

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 module.exports = {
   withBundleAnalyzer,
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: false,
   images: {
     domains: ["kalanidhithemes.com"],
   },
